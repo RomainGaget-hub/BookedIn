479a57f0a0aa2cdf12d7440e3c24ce5d813e6920
romaingaget <romaingaget@gmail.com>
2024-09-28 18:54:11 +0100

    next-auth init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Sidebar from "./components/Sidebar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
